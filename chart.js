cdfdf3d6b41777a546e8a1b65b8b7324cb9f2b1d
Faik Toplu <faiktoplu@faiks-mbp.home>
2023-06-15 00:37:36 +0200

    Texte plus graphique cyber ajouter

--- chart.js
+++ chart.js
@@ -1,16 +1,16 @@
 const data = {
   labels: [
-    'Red',
-    'Yellow',
-    'Blue'
+    'Autres atteintes',
+    'Violence physique',
+    'Violence verbale'
   ],
   datasets: [{
     label: 'My First Dataset',
     data: [12.8, 36.2, 50.7],
     backgroundColor: [
-      'rgb(204, 0, 0)',
-      'rgb(255, 255, 51)',
-      'rgb(0, 102, 204)'
+      'rgb(201, 36, 43)',
+      'rgb(250, 199, 17)',
+      'rgb(0, 159, 227)'
     ],
     borderColor: '#000',
     borderWidth: 3,
