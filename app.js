2b0a2308bf0601475bd1848eed6c90a3b52e7169
Faik Toplu <faiktoplu@faiks-mbp.home>
2023-06-18 17:58:13 +0200

    Version 5 finaliser sans responsive

--- app.js
+++ app.js
@@ -13,6 +13,47 @@ document.addEventListener("scroll", function(){
     if (window.scrollY>1200){
         horloge.innerHTML=10;
     }
+
+    if (window.scrollY>1400){
+        horloge.innerHTML=11;
+    }
+
+    if (window.scrollY>1600){
+        horloge.innerHTML=12;
+    }
      
+    if (window.scrollY>1800){
+        horloge.innerHTML=13;
+    }
+
+    if (window.scrollY>2000){
+        horloge.innerHTML=14;
+    }
+
+    if (window.scrollY>2200){
+        horloge.innerHTML=15;
+    }
+
+    if (window.scrollY>2400){
+        horloge.innerHTML=16;
+    }
+
+    if (window.scrollY>2600){
+        horloge.innerHTML=17;
+    }
+
+    if (window.scrollY>2800){
+        horloge.innerHTML=18;
+    }
+
+    if (window.scrollY>3000){
+        horloge.innerHTML=19;
+    }
+
+    if (window.scrollY>3300){
+        horloge.innerHTML;
+    }
+
+
 })
 
